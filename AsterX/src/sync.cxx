b99a7aad47102805469a30089f0c679af224ed94
Liwei Ji <jiliwei.phys@gmail.com>
2025-08-20 15:54:11 -0400

    AsterX: remove sync of fluxes

--- AsterX/src/sync.cxx
+++ AsterX/src/sync.cxx
@@ -12,32 +12,10 @@ using namespace CarpetX;
 ////////////////////////////////////////////////////////////////////////////////
 
 extern "C" void AsterX_Sync(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_ARGUMENTSX_AsterX_Sync;
-
   // do nothing
 }
 
-extern "C" void AsterX_ApplyOuterBCOnPrim(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_PARAMETERS;
-
-  std::vector<int> groups;
-
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::rho"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::vel"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::eps"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::press"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::Bvec"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::temperature"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::entropy"));
-  groups.push_back(CCTK_GroupIndex("HydroBaseX::Ye"));
-
-  groups.push_back(CCTK_GroupIndex("AsterX::zvec"));
-  groups.push_back(CCTK_GroupIndex("AsterX::svec"));
-
-  groups.push_back(CCTK_GroupIndex("AsterX::dBx_stag"));
-  groups.push_back(CCTK_GroupIndex("AsterX::dBy_stag"));
-  groups.push_back(CCTK_GroupIndex("AsterX::dBz_stag"));
-
+void ApplyOuterBC(CCTK_ARGUMENTS, std::vector<int> &groups) {
   task_manager tasks1;
   task_manager tasks2;
 
@@ -67,6 +45,49 @@ extern "C" void AsterX_ApplyOuterBCOnPrim(CCTK_ARGUMENTS) {
   synchronize();
 
   assert(ghext->num_patches() == 1);
+}
+
+extern "C" void AsterX_ApplyOuterBCOnPrim(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_PARAMETERS;
+
+  std::vector<int> groups;
+
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::rho"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::vel"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::eps"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::press"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::Bvec"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::temperature"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::entropy"));
+  groups.push_back(CCTK_GroupIndex("HydroBaseX::Ye"));
+
+  groups.push_back(CCTK_GroupIndex("AsterX::zvec"));
+  groups.push_back(CCTK_GroupIndex("AsterX::svec"));
+
+  groups.push_back(CCTK_GroupIndex("AsterX::dBx_stag"));
+  groups.push_back(CCTK_GroupIndex("AsterX::dBy_stag"));
+  groups.push_back(CCTK_GroupIndex("AsterX::dBz_stag"));
+
+  ApplyOuterBC(CCTK_PASS_CTOC, groups);
+}
+
+extern "C" void AsterX_ApplyOuterBCOnFluxes(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_PARAMETERS;
+
+  std::vector<int> groups;
+
+  groups.push_back(CCTK_GroupIndex("AsterX::flux_x"));
+  groups.push_back(CCTK_GroupIndex("AsterX::flux_y"));
+  groups.push_back(CCTK_GroupIndex("AsterX::flux_z"));
+
+  groups.push_back(CCTK_GroupIndex("AsterX::vtilde_xface"));
+  groups.push_back(CCTK_GroupIndex("AsterX::vtilde_yface"));
+  groups.push_back(CCTK_GroupIndex("AsterX::vtilde_zface"));
+  groups.push_back(CCTK_GroupIndex("AsterX::a_xface"));
+  groups.push_back(CCTK_GroupIndex("AsterX::a_yface"));
+  groups.push_back(CCTK_GroupIndex("AsterX::a_zface"));
+
+  ApplyOuterBC(CCTK_PASS_CTOC, groups);
 }
 
 } // namespace AsterX
