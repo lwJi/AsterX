3ac821ac8a7592f45c8a02dd7392ba672bb33c44
Liwei Ji <jiliwei.phys@gmail.com>
2025-09-09 14:55:51 -0400

    AsterUtils: unify calc_fd_c2c

--- AsterUtils/src/aster_fd.hxx
+++ AsterUtils/src/aster_fd.hxx
@@ -6,9 +6,9 @@
 #include <cctk_Parameters.h>
 
 #include <mat.hxx>
-#include <vec.hxx>
-#include <sum.hxx>
 #include <simd.hxx>
+#include <sum.hxx>
+#include <vec.hxx>
 
 #include <algorithm>
 #include <array>
@@ -29,13 +29,6 @@ calc_fd2_v2v_oneside(const GF3D2<const T> &gf, const PointDesc &p,
          (gf(p.I + 2 * sign * p.DI[dir]) - 4.0 * gf(p.I + sign * p.DI[dir]) +
           3.0 * gf(p.I)) *
          (0.5 / p.DX[dir]);
-}
-
-// FD2: cell centered input, cell centered output
-template <typename T>
-CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T
-calc_fd2_c2c(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
-  return (0.5 / p.DX[dir]) * (gf(p.I + p.DI[dir]) - gf(p.I - p.DI[dir]));
 }
 
 // FD2: vertex centered input, edge centered output
@@ -67,15 +60,6 @@ CCTK_DEVICE CCTK_HOST
           gf(p.I + p.DI[dir1] + p.DI[dir2])) /
          p.DX[dir];
   return 0.25 * (dgf1 + dgf2 + dgf3 + dgf4);
-}
-
-// FD4: cell centered input, cell centered output
-template <typename T>
-CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T
-calc_fd4_c2c(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
-  return (1.0 / (12.0 * p.DX[dir])) *
-         (-gf(p.I + 2 * p.DI[dir]) + 8.0 * gf(p.I + p.DI[dir]) -
-          8.0 * gf(p.I - p.DI[dir]) + gf(p.I - 2 * p.DI[dir]));
 }
 
 // FD4: vertex centered input, cell centered output
@@ -113,6 +97,24 @@ CCTK_DEVICE CCTK_HOST
          p.DX[dir];
 
   return 0.25 * (dgf1 + dgf2 + dgf3 + dgf4);
+}
+
+// FD2: cell centered input, cell centered output
+template <int FDORDER, typename T>
+CCTK_DEVICE CCTK_HOST
+    CCTK_ATTRIBUTE_ALWAYS_INLINE inline std::enable_if_t<FDORDER == 2, T>
+    calc_fd_c2c(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
+  return (0.5 / p.DX[dir]) * (gf(p.I + p.DI[dir]) - gf(p.I - p.DI[dir]));
+}
+
+// FD4: cell centered input, cell centered output
+template <int FDORDER, typename T>
+CCTK_DEVICE CCTK_HOST
+    CCTK_ATTRIBUTE_ALWAYS_INLINE inline std::enable_if_t<FDORDER == 4, T>
+    calc_fd_c2c(const GF3D2<const T> &gf, const PointDesc &p, const int dir) {
+  return (1.0 / (12.0 * p.DX[dir])) *
+         (-gf(p.I + 2 * p.DI[dir]) + 8.0 * gf(p.I + p.DI[dir]) -
+          8.0 * gf(p.I - p.DI[dir]) + gf(p.I - 2 * p.DI[dir]));
 }
 
 } // namespace AsterUtils
