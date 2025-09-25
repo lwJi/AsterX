d6683626c0dfa6aae5d7a179a7b4009085ea43a1
Liwei Ji <jiliwei.phys@gmail.com>
2025-09-25 10:54:23 -0400

    AsterX: add small aux function hll_upwind

--- AsterX/src/rhs.cxx
+++ AsterX/src/rhs.cxx
@@ -15,6 +15,16 @@ using namespace AsterUtils;
 
 enum class vector_potential_gauge_t { algebraic, generalized_lorentz };
 
+template <typename T>
+CCTK_DEVICE CCTK_ATTRIBUTE_ALWAYS_INLINE static inline T
+hll_upwind(T uL, T uR, T fL, T fR, T ap, T am) {
+  const T s = ap + am;
+  if (s <= T(1e-14)) {
+    return T(0.5) * (fL + fR);
+  }
+  return (ap * fL + am * fR - ap * am * (uR - uL)) / s;
+}
+
 template <int i>
 void CalcRHSofAvec(CCTK_ARGUMENTS, const vector_potential_gauge_t gauge,
                    const reconstruction_t reconstruction,
@@ -34,8 +44,7 @@ void CalcRHSofAvec(CCTK_ARGUMENTS, const vector_potential_gauge_t gauge,
 
   // upwind-CT
   const vec<GF3D2<const CCTK_REAL>, dim> gf_vels{velx, vely, velz};
-  const vec<GF3D2<const CCTK_REAL>, dim> dBstag_j{dBy_stag, dBz_stag, dBx_stag};
-  const vec<GF3D2<const CCTK_REAL>, dim> dBstag_k{dBz_stag, dBx_stag, dBy_stag};
+  const vec<GF3D2<const CCTK_REAL>, dim> dBstag{dBx_stag, dBy_stag, dBz_stag};
   // j-comp reconstructed in k-dir
   const vec<GF3D2<const CCTK_REAL>, dim> vbars_j_reck{
       vbar_y_zface, vbar_z_xface, vbar_x_yface};
@@ -61,34 +70,37 @@ void CalcRHSofAvec(CCTK_ARGUMENTS, const vector_potential_gauge_t gauge,
         CCTK_REAL E;
 
         if (use_uct) { // upwind-CT
+          // reconstruct in k-dir
           const vec<CCTK_REAL, 2> dBstag_j_reck_rc{
-              reconstruct(dBstag_j(i), p, reconstruction, k, false, false,
-                          press, gf_vels(k), reconstruct_params)};
-          const vec<CCTK_REAL, 2> dBstag_k_recj_rc{
-              reconstruct(dBstag_k(i), p, reconstruction, j, false, false,
-                          press, gf_vels(j), reconstruct_params)};
-          const vec<CCTK_REAL, 2> vbars_j_reck_recj_rc{
-              reconstruct(vbars_j_reck(i), p, reconstruction, j, false, false,
-                          press, gf_vels(j), reconstruct_params)};
+              reconstruct(dBstag(j), p, reconstruction, k, false, false, press,
+                          gf_vels(k), reconstruct_params)};
           const vec<CCTK_REAL, 2> vbars_k_recj_reck_rc{
               reconstruct(vbars_k_recj(i), p, reconstruction, k, false, false,
                           press, gf_vels(k), reconstruct_params)};
+          // reconstruct in j-dir
+          const vec<CCTK_REAL, 2> dBstag_k_recj_rc{
+              reconstruct(dBstag(k), p, reconstruction, j, false, false, press,
+                          gf_vels(j), reconstruct_params)};
+          const vec<CCTK_REAL, 2> vbars_j_reck_recj_rc{
+              reconstruct(vbars_j_reck(i), p, reconstruction, j, false, false,
+                          press, gf_vels(j), reconstruct_params)};
+
+          const CCTK_REAL ap_k = amax_reck(i)(p.I);
+          const CCTK_REAL am_k = amin_reck(i)(p.I);
+          const CCTK_REAL ap_j = amax_recj(i)(p.I);
+          const CCTK_REAL am_j = amin_recj(i)(p.I);
 
           // first term
-          const CCTK_REAL amax_k = amax_reck(i)(p.I);
-          const CCTK_REAL amin_k = amin_reck(i)(p.I);
-          E = (amax_k * vbars_k_recj_reck_rc(0) * dBstag_j_reck_rc(0) +
-               amin_k * vbars_k_recj_reck_rc(1) * dBstag_j_reck_rc(1) -
-               amax_k * amin_k * (dBstag_j_reck_rc(1) - dBstag_j_reck_rc(0))) /
-              (amax_k + amin_k);
+          E = hll_upwind(dBstag_j_reck_rc(0), dBstag_j_reck_rc(1),
+                         vbars_k_recj_reck_rc(0) * dBstag_j_reck_rc(0),
+                         vbars_k_recj_reck_rc(1) * dBstag_j_reck_rc(1), ap_k,
+                         am_k);
 
           // second term
-          const CCTK_REAL amax_j = amax_recj(i)(p.I);
-          const CCTK_REAL amin_j = amin_recj(i)(p.I);
-          E -= (amax_j * vbars_j_reck_recj_rc(0) * dBstag_k_recj_rc(0) +
-                amin_j * vbars_j_reck_recj_rc(1) * dBstag_k_recj_rc(1) -
-                amax_j * amin_j * (dBstag_k_recj_rc(1) - dBstag_k_recj_rc(0))) /
-               (amax_j + amin_j);
+          E -= hll_upwind(dBstag_k_recj_rc(0), dBstag_k_recj_rc(1),
+                          vbars_j_reck_recj_rc(0) * dBstag_k_recj_rc(0),
+                          vbars_j_reck_recj_rc(1) * dBstag_k_recj_rc(1), ap_j,
+                          am_j);
         } else { // flux-CT
           E = 0.25 * ((gf_fBs(j)(k)(p.I) + gf_fBs(j)(k)(p.I - p.DI[j])) -
                       (gf_fBs(k)(j)(p.I) + gf_fBs(k)(j)(p.I - p.DI[k])));
